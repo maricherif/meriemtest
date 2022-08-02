fc9cf3136d36956fa21f4768824406e54b929153
marie-cherif <maremesow422@gmail.com>
2022-08-02 21:09:15 +0000

    programme pour la somme

---
+++ sectest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b, s;
+    printf("donne les  nombre ");
+    scanf("%d %d", &a,&b);
+    s=a+b;
+    printf("la somme est = %d",s);
+    return 0;
+}
